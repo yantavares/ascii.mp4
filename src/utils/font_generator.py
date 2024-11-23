3436121ac6ef11d71dcfd2d7cb9997d63d15c85a
yantavares <yantdo1@gmail.com>
2024-11-22 23:28:07 -0300

    Refactor font and video generation utilities for improved readability

--- src/utils/font_generator.py
+++ src/utils/font_generator.py
@@ -5,7 +5,6 @@ import shutil
 
 
 def textsize(text, font):
-    """Get dimensions of the given text when rendered in the given font."""
     im = Image.new(mode="L", size=(0, 0))
     draw = ImageDraw.Draw(im)
     _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
@@ -13,30 +12,24 @@ def textsize(text, font):
 
 
 def create_images_from_font(font_path, output_dir, characters, image_size=(10, 10), font_size=10):
-    # Load the font
     font = ImageFont.truetype(font_path, font_size)
 
-    # Create output directory if it doesn't exist
     if os.path.exists(output_dir):
         print(f"Removing existing directory: {output_dir}")
         shutil.rmtree(output_dir)
 
     os.makedirs(output_dir)
 
-    # Generate an image for each character
     for char in characters:
-        img = Image.new('L', image_size, color=255)  # white background
+        img = Image.new('L', image_size, color=255)
         d = ImageDraw.Draw(img)
-        # Calculate text position: centered
-        width, height = textsize(char, font)  # Using updated textsize function
+        width, height = textsize(char, font)
         position = ((image_size[0] - width) / 2, (image_size[1] - height) / 2)
         d.text(position, char, font=font, fill=0)  # black text
 
-        # Save the image using ASCII value to avoid file name issues with special characters
         img.save(os.path.join(output_dir, f"{ord(char)}.png"))
 
 
-# Usage example
 font = 'ComicMono'
 
 if len(sys.argv) > 1:
@@ -45,10 +38,10 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
     font_size = int(sys.argv[2].replace(' ', ''))
 
-font_path = f'fonts/{font}.ttf'  # Path to the TTF font file
-output_dir = f'fonts/{font}_chars'  # Directory to save letter images
-# Characters to render
-characters = '@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^` ' 
+font_path = f'fonts/{font}.ttf'
+output_dir = f'fonts/{font}_chars'
+
+characters = '@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^` '
 
 create_images_from_font(font_path, output_dir, characters,
                         (font_size, font_size), font_size)
--- src/utils/video_generator.py
+++ src/utils/video_generator.py
@@ -3,29 +3,26 @@ import os
 
 
 def create_video_from_frames(frame_dir, output_video_path, fps=24):
-    # Get the list of frame files sorted by frame number
+
     frame_files = [os.path.join(frame_dir, f) for f in sorted(
         os.listdir(frame_dir)) if f.endswith('.png')]
-    # Read the first frame to determine the size
+
     frame = cv2.imread(frame_files[0])
     height, width, layers = frame.shape
 
-    # Define the codec and create a VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can also use 'XVID'
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 
-    # Read each file and add it to the video
     for file in frame_files:
         frame = cv2.imread(file)
-        video.write(frame)  # Add frame to video
+        video.write(frame)
 
-    video.release()  # Release the video writer
+    video.release()
 
 
-# Directory and video settings
-frame_dir = 'output/frames'  # Directory containing frames
-# Desired path for the output video
+frame_dir = 'output/frames'
+
 output_video_path = 'output/text.mp4'
-fps = 24  # Frames per second of the output video
+fps = 24
 
 create_video_from_frames(frame_dir, output_video_path, fps)
